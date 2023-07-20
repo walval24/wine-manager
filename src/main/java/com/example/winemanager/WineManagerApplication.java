6ebc7d95dffd662bee824d33b1dbcde74ab726fb
Walter Valentini <walval@outlook.it>
2023-07-20 19:43:01 +0200

    first commit

---
+++ src/main/java/com/example/winemanager/WineManagerApplication.java
@@ -0,0 +1,16 @@
+package com.example.winemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineManagerApplication {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(WineManagerApplication.class, args);
+	}
+
+
+}
